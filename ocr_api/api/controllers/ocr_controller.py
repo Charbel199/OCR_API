f4493e9c91e2ac8e324ae3c4955f3d3905f895bc
Charbel199 <charbel-boumaroun@outlook.com>
2021-12-06 18:14:06 +0200

    Add character extraction endpoints

--- ocr_api/api/controllers/ocr_controller.py
+++ ocr_api/api/controllers/ocr_controller.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, HTTPException
-from typing import List
+from fastapi import APIRouter, HTTPException, UploadFile, File
+from fastapi.responses import StreamingResponse
 from domain.exceptions.application_error import ApplicationError
 from containers import Services
 
@@ -8,11 +8,21 @@ character_extractor_service = Services.character_extractor_service()
 
 
 @router.post('/content')
-async def get_image_content() -> List[List[str]]:
+async def get_image_content(image: UploadFile = File(...)) -> str:
     try:
-        return character_extractor_service.get_characters_info()
+        return character_extractor_service.get_characters_info(await image.read())
     except ApplicationError as e:
         raise HTTPException(status_code=400, detail=e.__str__())
     except Exception as e:
         raise HTTPException(status_code=500, detail=e.__str__())
 
+
+@router.post('/characters')
+async def get_characters_bounding_box(image: UploadFile = File(...)) -> StreamingResponse:
+    try:
+        image_bytes = character_extractor_service.get_characters_bounding_boxes(await image.read())
+        return StreamingResponse(content=image_bytes, media_type="image/png")
+    except ApplicationError as e:
+        raise HTTPException(status_code=400, detail=e.__str__())
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e.__str__())
